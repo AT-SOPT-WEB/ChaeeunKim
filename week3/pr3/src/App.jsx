72ca3c1e8055b22cd85de77800c73c0992ec17d0
bongtta <kce0419@naver.com>
2025-04-26 15:23:17 +0900

    feat: 3주차 실습

---
+++ week3/pr3/src/App.jsx
@@ -0,0 +1,24 @@
+import Card from "./components/Card";
+import Header from "./components/Header";
+
+import { members } from "./constants/members";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <section style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+        {members.map((member) => (
+          <Card
+            key={member.id}
+            name={member.name}
+            github={member.github}
+            englishName={member.englishName}
+          />
+        ))}
+      </section>
+    </>
+  );
+}
+
+export default App;
